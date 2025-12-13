52f7f2141d5c0671c708b45af2de48639dd3109d
Hipslumb <marykolesnikova139@gmail.com>
2025-12-13 17:19:37 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <windows.h>
 using std::cout;
 
 int main() {
-	setlocale(LC_ALL, "ru_RU.UTF-8");
-	cout << "Привет!";
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    cout << u8"Привет Мир!";
 	return 0;
 }
